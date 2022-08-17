86a544d6406e771a118630d879773b5d19a8a6c8
carlosavanzini <avanzinidesarrollo@gmail.com>
2022-08-17 10:49:34 -0300

    segundo commit

--- javascript.js
+++ javascript.js
@@ -95,87 +95,109 @@ Deberá de repetirse el proceso hasta que el usuario pulse «cancelar».
 
 */
 
-do {
-  let dni = parseInt(prompt("Ingrese su dni"));
-  if (isNaN(dni)) {
-    alert("No ingreso un numero");
-  }  else if (dni >= 0 && dni <= 99999999) {
-        resto = dni % 23;
-    switch (resto) {
-      case 0:
-        letra = "T";
-        break;
-      case 1:
-        letra = "R";
-        break;
-      case 2:
-        letra = "W";
-        break;
-      case 3:
-        letra = "A";
-        break;
-      case 4:
-        letra = "G";
-        break;
-      case 5:
-        letra = "M";
-        break;
-      case 6:
-        letra = "Y";
-        break;
-      case 7:
-        letra = "F";
-        break;
-      case 8:
-        letra = "P";
-        break;
-      case 9:
-        letra = "D";
-        break;
-      case 10:
-        letra = "X";
-        break;
-      case 11:
-        letra = "B";
-        break;
-      case 12:
-        letra = "N";
-        break;
-      case 13:
-        letra = "J";
-        break;
-      case 14:
-        letra = "Z";
-        break;
-      case 15:
-        letra = "S";
-        break;
-      case 16:
-        letra = "Q";
-        break;
-      case 17:
-        letra = "V";
-        break;
-      case 18:
-        letra = "H";
-        break;
-      case 19:
-        letra = "L";
-        break;
-      case 20:
-        letra = "C";
-        break;
-      case 21:
-        letra = "K";
-        break;
-      case 22:
-        letra = "E";
-        break;
-      default:
-        alert("Numero erroneo");
-    }
-    alert(`su dni es igual a ${dni} y la letra que le corresponde es ${letra}`);
-  }else{
-    alert("El numero esta fuera de rango, recuerde que es desde 0 a 99999999 ");}
+// do {
+//   let dni = parseInt(prompt("Ingrese su dni"));
+//   if (isNaN(dni)) {
+//     alert("No ingreso un numero");
+//   }  else if (dni >= 0 && dni <= 99999999) {
+//         resto = dni % 23;
+//     switch (resto) {
+//       case 0:
+//         letra = "T";
+//         break;
+//       case 1:
+//         letra = "R";
+//         break;
+//       case 2:
+//         letra = "W";
+//         break;
+//       case 3:
+//         letra = "A";
+//         break;
+//       case 4:
+//         letra = "G";
+//         break;
+//       case 5:
+//         letra = "M";
+//         break;
+//       case 6:
+//         letra = "Y";
+//         break;
+//       case 7:
+//         letra = "F";
+//         break;
+//       case 8:
+//         letra = "P";
+//         break;
+//       case 9:
+//         letra = "D";
+//         break;
+//       case 10:
+//         letra = "X";
+//         break;
+//       case 11:
+//         letra = "B";
+//         break;
+//       case 12:
+//         letra = "N";
+//         break;
+//       case 13:
+//         letra = "J";
+//         break;
+//       case 14:
+//         letra = "Z";
+//         break;
+//       case 15:
+//         letra = "S";
+//         break;
+//       case 16:
+//         letra = "Q";
+//         break;
+//       case 17:
+//         letra = "V";
+//         break;
+//       case 18:
+//         letra = "H";
+//         break;
+//       case 19:
+//         letra = "L";
+//         break;
+//       case 20:
+//         letra = "C";
+//         break;
+//       case 21:
+//         letra = "K";
+//         break;
+//       case 22:
+//         letra = "E";
+//         break;
+//       default:
+//         alert("Numero erroneo");
+//     }
+//     alert(`su dni es igual a ${dni} y la letra que le corresponde es ${letra}`);
+//   }else{
+//     alert("El numero esta fuera de rango, recuerde que es desde 0 a 99999999 ");}
 
-  } while (confirm("Desea cargar otro dni?"));
+//   } while (confirm("Desea cargar otro dni?"));
+
+/*
+6- Realiza un script que escriba una pirámide del 1 al 30 de la siguiente forma :
+
+1
+22
+333
+4444
+55555
+666666
+…….
+
+*/
+
+// let i, repeticion;
+// for (i = 1; i <= 30; i++) {
+//   for (repeticion = 0; repeticion < i; repeticion++) {
+//     document.write(i);
+//   }
+
+//   document.write("<br>");
+// }
